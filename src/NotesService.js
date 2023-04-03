4939fbbfe6312c42c7c5d609f708b3356a64a811
Jack Driscoll <jdriscoll98@ufl.edu>
2023-04-03 00:51:48 -0400

    Add access control headers

--- src/NotesService.js
+++ src/NotesService.js
@@ -8,14 +8,47 @@ export default class NotesService {
   }
 
   static async createNote(note) {
-    return axios.post(`${API_URL}/notes`, note);
+    try {
+      const response = await axios.post(`${API_URL}/notes`, note, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   static async updateNote(id, note) {
-    return axios.put(`${API_URL}/notes/${id}`, note);
+    try {
+      const response = await axios.put(`${API_URL}/notes/${id}`, note, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   static async deleteNote(id) {
-    return axios.delete(`${API_URL}/notes/${id}`);
+    try {
+      const response = await axios.delete(`${API_URL}/notes/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
